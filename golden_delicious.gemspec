a99c3516a36f9f8e285f41947575ade1f0d57e29
Kenneth Pullen <kenneth.pullen@gmail.com>
2012-08-08 00:51:51 -0700

    Clean up GoldenDelicious and add Nokogiri as a dependency
    Nokogiri will be used when GoldenDelicious hooks up with
    everymac.com; I'm just adding it because I'll probably forget. Also, I cleaned up GoldenDelicious a little bit, and added
    some documentation.

--- golden_delicious.gemspec
+++ golden_delicious.gemspec
@@ -17,5 +17,6 @@ Gem::Specification.new do |gem|
 
   gem.add_development_dependency 'rspec'
   gem.add_dependency 'httparty'
-  gem.add_dependency 'crack'
+  gem.add_dependency 'json'
+  gem.add_dependency 'nokogiri'
 end
--- lib/golden_delicious.rb
+++ lib/golden_delicious.rb
@@ -4,13 +4,25 @@ require 'json'
 
 class GoldenDelicious
 	include HTTParty
-	base_uri 'https://selfsolve.apple.com'
 	def initialize dirty_serial_number
 		@sn = dirty_serial_number
 		@valid = nil
 	end
+
+	# Use Apple's warranty checking web service to determine if a serial number is valid.
 	def valid?
-		response = JSON.parse self.class.get('/warrantyChecker.do', :sn => @sn).body[5..-2]
+		dirty_response = self.class.get 'https://selfsolve.apple.com/warrantyChecker.do', :sn => @sn
+		
+		# Apple's web service for checking the validity of a serial number returns a JSON
+		# payload wrapped in a javascript function call: null(). The magic numbers in the
+		# array slice below remove the function call since the JSON module does not know
+		# to deal with it.
+		response = JSON.parse dirty_response.body[5..-2]
+
+		# Most of the keys in the response are all CAPS, which is just rude.
+		# TODO: Find a cleaner way of modifying Hash keys.
+		response = Hash[response.map {|k, v| [k.downcase, v]}]
+
 		false
 	end
 end
