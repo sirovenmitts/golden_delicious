3a6826259d0dd500c54fce470ea7d80362549dab
Kenneth Pullen <kenneth.pullen@gmail.com>
2012-08-08 19:05:31 -0700

    Create a GoldenDelicious object
    The GoldenDelicious object just pulls the AppleInfo and Stats
    objects together and delegates to them. Use this when you want
    both a serial number AND the cool stats from everymac.com

--- lib/golden_delicious/version.rb
+++ lib/golden_delicious/version.rb
@@ -1,3 +1,3 @@
-class GoldenDelicious
+module GoldenDelicious
   VERSION = "0.0.1"
 end
--- spec/golden_delicious_spec.rb
+++ spec/golden_delicious_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
 require 'golden_delicious'
 
-describe GoldenDelicious do
-	it 'should validate valid serial numbers' do
-		valid = GoldenDelicious.new 'W8734T8GZ5W'
-		valid.valid?.should eq true
+describe GoldenDelicious::GoldenDelicious do
+	before :all do
+		@gd = GoldenDelicious::GoldenDelicious.new 'W8734T8GZ5W'
 	end
-	it 'should not validate invalid serial numbers' do
-		invalid = GoldenDelicious.new 'W88401231AX'
-		invalid.valid?.should eq false
+	it 'should turn into a serial number when inspected' do
+		@gd.to_s.should eq 'W8734T8GZ5W'
 	end
-	it 'should remove the superfluous "S" prepended by some serial number scanners' do
-		sn = GoldenDelicious.new 'SW88401231AX'
-		sn.to_s.should eq 'W88401231AX'
+	it 'should delegate serial number validity testing to AppleInfo' do
+		@gd.valid?.should eq true
+	end
+	# In the interest of not repeating tests I am only going to test
+	# 1 value from the Stats object; if you want to know if the Stats
+	# object works, visit stats_spec.rb.
+	# NOTE: This could be a bad idea, since this is technically not
+	# testing the Stats object, but rather delegation.
+	it 'should delegate to Stats' do
+		@gd.identifiers.model.should eq 'A1181'
 	end
 end
