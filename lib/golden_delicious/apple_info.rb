dc1629947d04245832145b794551cf214af1c8ca
Kenneth Pullen <kenneth.pullen@gmail.com>
2012-08-08 18:41:26 -0700

    Extract AppleInfo
    GoldenDelicious is more than just a serial number validator;
    it also pulls information from everymac.com. It makes sense to
    pull these out into separate classes, since you may want one
    without the other.

---
+++ lib/golden_delicious/apple_info.rb
@@ -0,0 +1,44 @@
+require 'httparty'
+require 'memoist'
+require 'json'
+
+class AppleInfo
+	include HTTParty
+	extend Memoist
+	def initialize dirty_serial_number
+		# Some USB scanners prepend an 'S' to every Apple serial number. This does not happen
+		# with other serial numbers, so I'm assuming it has something to do with the way Apple
+		# prints their serial number barcodes. Anyhow, Apple will reject a serial number with
+		# the 'S', so I better remove it.
+		dirty_serial_number.slice! 0 if dirty_serial_number[0] == 'S'
+		@sn = dirty_serial_number
+	end
+
+	# Use Apple's warranty checking web service to determine if a serial number is valid.
+	def valid?
+		dirty_response = self.class.get 'https://selfsolve.apple.com/warrantyChecker.do', :query => {:sn => @sn}
+		
+		# Apple's web service for checking the validity of a serial number returns a JSON
+		# payload wrapped in a javascript function call: null(). The magic numbers in the
+		# array slice below remove the function call since the JSON module does not know
+		# to deal with it.
+		response = JSON.parse dirty_response.body[5..-2]
+
+		# Most of the keys in the response are all CAPS, which is just rude.
+		# TODO: Find a cleaner way of modifying Hash keys.
+		response = Hash[response.map {|k, v| [k.downcase, v]}]
+
+		# Apple returns an error hash if unable to find the serial number.
+		# TODO: So far I have not seen an error key returned if the serial number was located. This may
+		# change in the future. Find some more reliable key in the valid serial number response.
+		!response.has_key?('error_code')
+	end
+
+	# Apple's SelfSolve can take a long time to respond.
+	memoize :valid?
+
+	# Return this serial numer from whence it came (a String)
+	def to_s
+		@sn
+	end
+end
