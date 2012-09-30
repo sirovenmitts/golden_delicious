d05bac05637d95fd698c9efac57ceb6b746db43b
Kenneth Pullen <kenneth.pullen@gmail.com>
2012-09-30 16:05:33 -0700

    Remove AppleInfo
    Apple changed the way the web app at selfsolve.apple.com works. I have
    been unable to fetch data from the site without using a browser.

--- lib/golden_delicious/golden_delicious.rb
+++ lib/golden_delicious/golden_delicious.rb
@@ -2,7 +2,6 @@ require 'forwardable'
 module GoldenDelicious
 	class GoldenDelicious
 		extend Forwardable
-		def_delegators :@apple_info, :to_s, :valid?, :in_warranty?, :has_applecare?
 		def_delegators :@everymac_info, :memory, :storage, :identifiers
 		def initialize dirty_serial_number
 			# Some USB scanners prepend an 'S' to every Apple serial number. This does not happen
@@ -11,7 +10,7 @@ module GoldenDelicious
 			# the 'S', so I better remove it.
 			dirty_serial_number.slice! 0 if dirty_serial_number[0] == 'S'
 
-			@apple_info = AppleInfo.new dirty_serial_number
+			@serial_number = dirty_serial_number
 
 			# Apple embeds a short model number into every serial number. Serial numbers 11 digits
 			# long have 3 digits ids, and 12 digit serial numbers have a 4 digit id. This id is used
@@ -20,6 +19,12 @@ module GoldenDelicious
 			short_model.slice! 0 if dirty_serial_number.length == 11
 
 			@everymac_info = EverymacInfo.new short_model
+		end
+		def to_s
+			@serial_number
+		end
+		def valid?
+			true
 		end
 	end
 end
